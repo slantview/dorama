66fbac09662341a3f132a300150a4884fd265c05
Steve Rude <steve@slantview.com>
2012-06-04 20:10:17 -0700

    initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dorama::Application.config.secret_token = '6b6826509729d2fe4a48d651c83c555a5f74b0daa629371694d4a25a1913ed54491027c0f50e3dcaf69c0f7449703112ad5933599d5bb3f47246cf3d91c6700c'
