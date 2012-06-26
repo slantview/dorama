19f684fea733bef1e8db9af0aa0da6b046dc1978
Steve Rude <steve@slantview.com>
2012-06-26 12:41:56 -0700

    add stuff

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Dorama::Application.config.session_store :cookie_store, key: '_dorama_session'
+Dorama::Application.config.session_store :cookie_store, :key => '_dorama_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
